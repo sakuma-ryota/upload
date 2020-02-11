ab873c317eebaae242ce2c71da7bc2c3514e7928
sakuma <srff7cloud@yahoo.co.jp>
2020-02-11 06:20:57 +0000

    php課題11です。

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpをよみこむ　　--}}
+@extends('layouts.profile')
 
-        <title>WebpageCreate</title>
-    </head>
-    <body>
-        <h1>ウェブページ作成画面git</h1>
-    </body>
-</html>
+{{-- profile.blade.phpの@('title')にMy プロフィール'を埋め込むます' --}}
+@section('title', 'My プロフィール')
 
-  
+{{-- profile.blade.php@yield('content')に以下のタグを埋め込みます　--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>    
+        </div>
+    </div>
+@endsection
