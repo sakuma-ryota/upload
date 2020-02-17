11efd68252ab0de48b30ab30f07b39b6297c24ad
sakuma <srff7cloud@yahoo.co.jp>
2020-02-17 06:20:56 +0000

    php課題16提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,7 +16,8 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
         //Varidationを行う
         $this->validate($request, Profile::$rules);
         
@@ -33,11 +34,34 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
         return view('admin/profile/create');
     }
-    public function edit() {
-        return view('admin.profile.edit');
+    public function edit(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update() {
+    //データ更新アクション
+    public function update(Request $request)
+    {
+        //validtationをかける
+        $this->validtate($request, Profile::$rules);
+        //Profile Modelからデータを格納する
+        $profile = Profile::find($request->id);
+        //送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        //該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
         return redirect('admin/profile/edit');
+    }
+    //データの削除アクション
+    public function delete(Request $request)
+    {
+        //該当するProfile Modelを習得
+        $profile = Profile::find($request->id);
+        //削除する
+        $profile->delete();
+        return redirect('admin/profile/');
     }
 }
 
