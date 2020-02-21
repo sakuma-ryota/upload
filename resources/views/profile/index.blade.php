cd00615bfe49ff7e831926021c3949e2130a7a7b
”sakuma” <srff7cloud@yahoo.co.jp>
2020-02-21 10:15:17 +0000

    first commit

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,36 +1,46 @@
 @extends('layouts.front')
 
 @section('content')
-  <div class="container">
-    <hr color="#c0c0c0">
-    <div class="row">
-      <div class="posts col-md-8">
-        @foreach($posts as $post)
-          <div class="post">
-            <div class="row">
-              <div class="text col-md-8">
-                <div class="date">
-                  {{ $post->updated_at->format('Y年m月d日') }}
-                </div>
-                <div class="name">
-                  {{ str_limit($post->name, 150) }}
-                </div>
-                <div class="gender mt-3">
-                  {{ str_limit($post->gender, 100) }}
-                </div>
-                <div class="hobby mt-3">
-                  {{ str_limit($post->hobby, 1000) }}
-                </div>
-                <div class="introduction mt-3">
-                  {{ str_limit($post->introduction, 1500)}}
+    <div class="container">
+        <hr color="#c0c0c0">
+            <!--@if (!is_null($headline))-->
+            <!--  <div class="row">-->
+            <!--    <div class="headline col-md-10 mx-auto">-->
+            <!--      <div class="row">-->
+            <!--        <div class="col-md-6">-->
+            <!--          <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>-->
+            <!--        </div>-->
+            <!--      </div>-->
+            <!--    </div>-->
+            <!--  </div>-->
+            <!--@endif-->
+        <div class="row">
+          <div class="posts col-md-10 mx-auto">
+            @foreach($posts as $post)
+              <div class="post">
+                <div class="row">
+                  <div class="text col-md-8">
+                    <div class="date">
+                      {{ $post->updated_at->format('Y年m月d日') }}
+                    </div>
+                    <div class="name">
+                      {{ str_limit($post->name, 150) }}
+                    </div>
+                    <div class="gender mt-3">
+                      {{ str_limit($post->gender, 100) }}
+                    </div>
+                    <div class="hobby mt-3">
+                      {{ str_limit($post->hobby, 1000) }}
+                    </div>
+                    <div class="introduction mt-3">
+                      {{ str_limit($post->introduction, 2000)}}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+              <hr color="#c0c0c0">
+            @endforeach
           </div>
-          <hr color="#c0c0c0">
-        @endforeach
-      </div>
+        </div>
     </div>
-  </div>
 @endsection
-
