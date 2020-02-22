f91f7af7d012f6c2419e986061bd3e0ee6771151
”sakuma” <srff7cloud@yahoo.co.jp>
2020-02-22 12:54:07 +0000

    herokuへupload3回目

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
-    Route::get('/', 'NewsController@index');
+    Route::get('news', 'Admin\NewsController@index');
     //pofile
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
