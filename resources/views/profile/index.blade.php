fdd2c02a517481da76b4c7879e6e6bfa52fe4f75
”sakuma” <srff7cloud@yahoo.co.jp>
2020-02-22 14:14:35 +0000

    herokuへupload3回目

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,19 +2,23 @@
 
 @section('content')
     <div class="container">
-        <hr color="#c0c0c0">
-          @if (!is_null($headline))
-            <div class="row">
-              <div class="headline col-md-10 mx-auto">
-                <h2 class="mx-auto">最新の更新</h2>
-                <div class="row">
-                  <div class="col-md-6">
-                    <p class="body mx-auto">{{ str_limit($headline->name, 650) }}</p>
-                  </div>
+      <hr color="#c0c0c0">
+        @if (!is_null($headline))
+          <div class="row">
+            <div class="headline col-md-10 mx-auto">
+              <h2 class="mx-auto">最新の更新</h2>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="name mx-auto">{{ $headline->name }}</div>
+                  <div class="gender mx-auto mt-3">{{ $headline->gender }}</div>
+                  <div class="hobby mx-auto mt-3">{{ $headline->hobby }}</div>
+                  <div class="introduction mx-auto mt-3">{{ $headline->introduction }}</div>
                 </div>
               </div>
             </div>
-          @endif
+          </div>
+        @endif
+        <hr color="#c0c0c0">
         <div class="row">
           <div class="posts col-md-10 mx-auto">
             @foreach($posts as $post)
@@ -22,7 +26,7 @@
                 <div class="row">
                   <div class="text col-md-8">
                     <div class="date">
-                      {{ $post->updated_at->format('Y年m月d日') }}
+                      {{ $post->updated_at->format('Y年m月d日h時m分') }}
                     </div>
                     <div class="name">
                       {{ str_limit($post->name, 150) }}
