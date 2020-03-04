4ab133c7fd044f7a29c8f89efeaef0c64d47a65f
”sakuma” <srff7cloud@yahoo.co.jp>
2020-03-04 01:31:11 +0000

    herokugazouhyouzi

--- routes/web.php
+++ routes/web.php
@@ -42,21 +42,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-
-
-
-/*
-3.「http://XXXXXX.jp/XXX というアクセスが来たときに、
-  AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
-  
-Route::group(['prefix' => XXX], funtion()
-{
-    Rout::get('', 'AAA@bbb');
-});
-
-4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、
-  admin/profile/create にアクセスしたらProfileController の add Action に、
-  admin/profile/edit にアクセスしたら ProfileController の edit Action に
-  割り当てるように設定してください。
-*/
